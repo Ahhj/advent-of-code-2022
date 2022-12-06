02020ce600af0095d8856fa1a981d49c365d450e
Alistair Jones <alistair.hh.jones@gmail.com>
2022-12-06 22:24:29 +0000

    Refactor

--- src/solutions/day5.py
+++ src/solutions/day5.py
@@ -11,65 +11,57 @@ from src.utils.read_input import read_input_data
 
 DAY = 5
 
+
+def process_moves(moves_data):
+    moves = map(
+        curry(re.findall, r"move (\d+) from (\d) to (\d)"), moves_data.split("\n")
+    )
+    moves = chain.from_iterable(moves)
+    moves = list(moves)
+    return moves
+
+
+def process_stacks(crates_data):
+    rows = crates_data.split("\n")
+    indexes = list(rows.pop(-1)[1::4])
+
+    rows = map(lambda row: row[1::4], rows)
+    columns = zip(*rows)
+
+    stacks = OrderedDict({})
+    for i, col in zip(indexes, columns):
+        # Stacks are ordered left-to-right from top-to-bottom
+        stacks[i] = list(filter(None, map(str.strip, col)))
+
+    return stacks
+
+
+def rearrange_crates(stacks, moves, one_by_one=True):
+    stacks = deepcopy(stacks)
+    for n, i, j in moves:
+        n = int(n)
+
+        crates = stacks[i][:n][:: 1 - 2 * one_by_one]
+
+        # Move the crates
+        stacks[j] = crates + stacks[j]
+        stacks[i] = stacks[i][n:]
+
+    return stacks
+
+
 input_data = read_input_data(DAY)
 
-
 crates_data, moves_data = input_data.split("\n\n")
+stacks = process_stacks(crates_data)
+moves = process_moves(moves_data)
 
-moves = map(curry(re.findall, r"move (\d+) from (\d) to (\d)"), moves_data.split("\n"))
-moves = chain.from_iterable(moves)
-moves = list(moves)
+stacks_a = rearrange_crates(stacks, moves)
+top_level_concat = "".join([crates[0] for _, crates in stacks_a.items()])
+aocd.submit(top_level_concat, part="a", year=config.YEAR, day=DAY)
 
-rows = crates_data.split("\n")
-indexes = list(rows.pop(-1)[1::4])
+stacks_b = rearrange_crates(stacks, moves, one_by_one=False)
+top_level_concat = "".join([crates[0] for _, crates in stacks_b.items()])
+aocd.submit(top_level_concat, part="b", year=config.YEAR, day=DAY)
 
-rows = map(lambda row: row[1::4], rows)
-columns = zip(*rows)
-
-stacks = OrderedDict({})
-for i, col in zip(indexes, columns):
-    # Stacks are ordered left-to-right from top-to-bottom
-    stacks[i] = list(filter(None, map(str.strip, col)))
-
-# %% Part a
-
-
-def solve_a(stacks, moves):
-    stacks = deepcopy(stacks)
-
-    for n, i, j in moves:
-        n = int(n)
-
-        # Reverse order since crates are added 1-by-1
-        crates = stacks[i][:n][::-1]
-
-        # Move the crates
-        stacks[j] = crates + stacks[j]
-        stacks[i] = stacks[i][n:]
-
-    top_level_concat = "".join([crates[0] for _, crates in stacks.items()])
-    aocd.submit(top_level_concat, part="a", year=config.YEAR, day=DAY)
-
-
-solve_a(stacks, moves)
-# %% Part b
-
-
-def solve_b(stacks, moves):
-    stacks = deepcopy(stacks)
-    for n, i, j in moves:
-        n = int(n)
-
-        # No need to reverse order
-        crates = stacks[i][:n]
-
-        # Move the crates
-        stacks[j] = crates + stacks[j]
-        stacks[i] = stacks[i][n:]
-
-    top_level_concat = "".join([crates[0] for _, crates in stacks.items()])
-    aocd.submit(top_level_concat, part="b", year=config.YEAR, day=DAY)
-
-
-solve_b(stacks, moves)
 # %%
